9206249b17496b62ed3ba609978c1613039f146c
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2025-05-13 22:09:32 +0530

    Courses Page Updated

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -3,18 +3,31 @@ import React from 'react';
 export default function Courses() {
   return (
     <section className='section-class'>
-      <h2>Courses & Internships</h2>
+      <h1>Courses</h1>
       <p>Explore learning and career-building opportunities.</p>
-
-      <h3>Our Offerings</h3>
       <ul>
-        <li>Intro to Data for Social Good</li>
-        <li>Community Health Internship</li>
-        <li>AI for Rural Development</li>
+       <li><i>Certificate Course in Space Nutrition</i></li>
+      <li><i>Certificate Course in Sports Nutrition – Level 1</i></li>
+      <li><i>Certificate Course in Bioenergetics in Sports Nutrition – Level 2</i></li>
+      <li><i>Certificate Course in Introduction to Environmental Nutrition</i></li>
+      <li><i>Certificate Course in Microplastic and Human Health</i></li>
+      <li><i>Certificate in Introduction to Food & Nutrition Security</i></li>
+      <li><i>Certificate Course in Herbal Healing</i></li>
+      <li><i>Certificate Course in Community Nutrition & Population Health</i></li>
+      <li><i>Certificate Course in Preschool Nutrition</i></li>
+      <li><i>Certificate Course in Society & Social Determinants of Health</i></li>
+      <li><i>Certificate Course in Yogic Nutrition</i></li>
+      <li><i>Certificate Course in Nutrition & Health Assessment Methods</i></li>
+      <li><i>Certificate Course in Food Enrichment & Nutrient Fortification</i></li>
+      <li><i>Certificate Course in Introduction to Research Methodology</i></li>
+      <li><i>Short-Term Certificate Course in Preconception Nutrition</i></li>
+      <li><i>Short-Term Certificate Course in Food Product Development</i></li>
+
+
       </ul>
 
-      <p>To register, fill the form:</p>
-      <a href="https://forms.gle/course-registration" target="_blank" rel="noopener noreferrer">Course Registration Form</a>
+      <h3>To register, fill the form below :</h3>
+      <a href="https://forms.gle/8Y2632LaHf1iBvm57" target="_blank" rel="noopener noreferrer">Course Registration Form</a>
     </section>
   );
 }
